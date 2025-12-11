161231533d8d392363ed176515edd9135429e50b
Rueteezy <ruel.romeo.sintinta@gmail.com>
2025-12-12 04:33:31 +0800

    Subarray shorter solution

--- src/main/java/com/test/algo/SubarraySumEqualsK.java
+++ src/main/java/com/test/algo/SubarraySumEqualsK.java
@@ -1,10 +1,19 @@
 package com.test.algo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarraySumEqualsK {
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3,2,3};
+
+        int[] nums = {1, 2, 3, 2, 3};
         int k = 3;
+        System.out.println(firstSolution(nums, k));
+        System.out.println(subarraySum(nums, k));
+    }
+
+    public static int firstSolution(int[] nums, int k){
         int count = 0;
 
         for (int i = 0; i < nums.length; i++) {
@@ -18,6 +27,21 @@ public class SubarraySumEqualsK {
                 }
             }
         }
-        System.out.println(count);
+        return count;
     }
+
+    public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> prefixCount = new HashMap<>();
+        prefixCount.put(0, 1);
+        int sum = 0, count = 0;
+
+        for (int n : nums) {
+            sum += n;
+            count += prefixCount.getOrDefault(sum - k, 0);
+            prefixCount.put(sum, prefixCount.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
+
 }
